adc8833e6769aa4e9d2b5404b85a64f2c99ff5f8
tom_glb <goodMorning_glb@atguigu.com>
2020-08-08 18:54:38 +0800

    新增

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-wrapper">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout',
+  components: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
